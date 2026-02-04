7a05d192f5e4575890e566701a176770bb298e36
Jabir-05 <knock5jabir@gmail.com>
2026-02-05 00:28:01 +0530

    HTTP

---
+++ Servers/HTTP/index.js
@@ -0,0 +1,23 @@
+const http = require("http");
+
+const fs= require("fs");
+
+
+
+const myServer = http.createServer((req, res) => {
+const log = `${Date.now()}: ${req.url} New Req Rec\n`;
+    fs.appendFile("log.txt",log ,(err,data) =>{
+        switch(req.url){
+            case '/':  res.end("HomePage");
+            break;
+            case '/about' :  res.end("I am  Jabir Imteyaz");
+            break;
+            default:
+                res.end("404 Not Found");
+
+
+        }
+    });
+});
+
+myServer.listen(8000,() => console.log("Server Started!"));
